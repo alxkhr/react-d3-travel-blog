50a407ec433307af7c26d10e1e30452ee3fc72ec
Alexander Kehr <alexander.kehr@mobilexag.de>
2017-03-13 15:18:42 +0100

    styling :art:

--- src/main-page.js
+++ src/main-page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Globe from './globe';
 import Blog from './blog';
+import '../theme/main-page.scss';
 
 export default function MainPage() {
   return (
-    <div>
+    <div className="main-page">
+      <div className="main-page__title"><h1>Eli around the world</h1></div>
       <Globe />
       <Blog />
     </div>
